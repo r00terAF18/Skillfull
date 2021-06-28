1f444b98fafd403e48543d645a16ed1f744f818a
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2021-06-28 13:27:49 +0430

    failsafe for random download clicks

--- Skillfull/Form1.Designer.cs
+++ Skillfull/Form1.Designer.cs
@@ -30,7 +30,6 @@ namespace Skillfull
         private void InitializeComponent()
         {
             this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
-            this.btnCopyCode = new MaterialSkin.Controls.MaterialFlatButton();
             this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
             this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
             this.cookieTxt = new MaterialSkin.Controls.MaterialSingleLineTextField();
@@ -38,6 +37,9 @@ namespace Skillfull
             this.statusStripLbl = new System.Windows.Forms.ToolStripStatusLabel();
             this.materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
             this.urlTxt = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialLabel5 = new MaterialSkin.Controls.MaterialLabel();
+            this.btnDownload = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btnCopyCode = new MaterialSkin.Controls.MaterialRaisedButton();
             this.statusStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,24 +55,6 @@ namespace Skillfull
             this.materialLabel1.Size = new System.Drawing.Size(307, 19);
             this.materialLabel1.TabIndex = 0;
             this.materialLabel1.Text = "Step 1: Paste this in your browsers console: ";
-            // 
-            // btnCopyCode
-            // 
-            this.btnCopyCode.AutoSize = true;
-            this.btnCopyCode.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnCopyCode.Depth = 0;
-            this.btnCopyCode.Icon = null;
-            this.btnCopyCode.Location = new System.Drawing.Point(474, 1);
-            this.btnCopyCode.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.btnCopyCode.MouseState = MaterialSkin.MouseState.HOVER;
-            this.btnCopyCode.Name = "btnCopyCode";
-            this.btnCopyCode.Primary = true;
-            this.btnCopyCode.Size = new System.Drawing.Size(95, 36);
-            this.btnCopyCode.TabIndex = 1;
-            this.btnCopyCode.Text = "Copy Code";
-            this.btnCopyCode.UseCompatibleTextRendering = true;
-            this.btnCopyCode.UseVisualStyleBackColor = true;
-            this.btnCopyCode.Click += new System.EventHandler(this.btnCopyCode_Click);
             // 
             // materialLabel2
             // 
@@ -112,25 +96,26 @@ namespace Skillfull
             this.cookieTxt.SelectionLength = 0;
             this.cookieTxt.SelectionStart = 0;
             this.cookieTxt.Size = new System.Drawing.Size(244, 23);
-            this.cookieTxt.TabIndex = 4;
+            this.cookieTxt.TabIndex = 2;
             this.cookieTxt.TabStop = false;
             this.cookieTxt.UseSystemPasswordChar = false;
+            this.cookieTxt.TextChanged += new System.EventHandler(this.cookieTxt_TextChanged);
             // 
             // statusStrip
             // 
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusStripLbl});
-            this.statusStrip.Location = new System.Drawing.Point(0, 501);
+            this.statusStrip.Location = new System.Drawing.Point(0, 179);
             this.statusStrip.Name = "statusStrip";
             this.statusStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.statusStrip.Size = new System.Drawing.Size(587, 22);
+            this.statusStrip.Size = new System.Drawing.Size(584, 22);
             this.statusStrip.TabIndex = 5;
             // 
             // statusStripLbl
             // 
             this.statusStripLbl.ActiveLinkColor = System.Drawing.Color.Salmon;
             this.statusStripLbl.Name = "statusStripLbl";
-            this.statusStripLbl.Size = new System.Drawing.Size(572, 17);
+            this.statusStripLbl.Size = new System.Drawing.Size(569, 17);
             this.statusStripLbl.Spring = true;
             // 
             // materialLabel4
@@ -159,30 +144,78 @@ namespace Skillfull
             this.urlTxt.SelectionLength = 0;
             this.urlTxt.SelectionStart = 0;
             this.urlTxt.Size = new System.Drawing.Size(244, 23);
-            this.urlTxt.TabIndex = 7;
+            this.urlTxt.TabIndex = 3;
             this.urlTxt.TabStop = false;
             this.urlTxt.UseSystemPasswordChar = false;
+            // 
+            // materialLabel5
+            // 
+            this.materialLabel5.AutoSize = true;
+            this.materialLabel5.Depth = 0;
+            this.materialLabel5.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.materialLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel5.Location = new System.Drawing.Point(12, 129);
+            this.materialLabel5.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel5.Name = "materialLabel5";
+            this.materialLabel5.Size = new System.Drawing.Size(166, 19);
+            this.materialLabel5.TabIndex = 8;
+            this.materialLabel5.Text = "Step 4: Start Download!";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.AutoSize = true;
+            this.btnDownload.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnDownload.Depth = 0;
+            this.btnDownload.Icon = null;
+            this.btnDownload.Location = new System.Drawing.Point(477, 129);
+            this.btnDownload.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Primary = true;
+            this.btnDownload.Size = new System.Drawing.Size(96, 36);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnCopyCode
+            // 
+            this.btnCopyCode.AutoSize = true;
+            this.btnCopyCode.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCopyCode.Depth = 0;
+            this.btnCopyCode.Icon = null;
+            this.btnCopyCode.Location = new System.Drawing.Point(477, 4);
+            this.btnCopyCode.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnCopyCode.Name = "btnCopyCode";
+            this.btnCopyCode.Primary = true;
+            this.btnCopyCode.Size = new System.Drawing.Size(95, 36);
+            this.btnCopyCode.TabIndex = 1;
+            this.btnCopyCode.Text = "Copy Code";
+            this.btnCopyCode.UseVisualStyleBackColor = true;
+            this.btnCopyCode.Click += new System.EventHandler(this.btnCopyCode_Click);
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(587, 523);
+            this.ClientSize = new System.Drawing.Size(584, 201);
+            this.Controls.Add(this.btnCopyCode);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.materialLabel5);
             this.Controls.Add(this.urlTxt);
             this.Controls.Add(this.materialLabel4);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.cookieTxt);
             this.Controls.Add(this.materialLabel3);
             this.Controls.Add(this.materialLabel2);
-            this.Controls.Add(this.btnCopyCode);
             this.Controls.Add(this.materialLabel1);
             this.Font = new System.Drawing.Font("Fira Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 240);
+            this.MinimumSize = new System.Drawing.Size(600, 240);
             this.Name = "mainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Skillfull";
-            this.Load += new System.EventHandler(this.mainForm_Load);
             this.statusStrip.ResumeLayout(false);
             this.statusStrip.PerformLayout();
             this.ResumeLayout(false);
@@ -193,7 +226,6 @@ namespace Skillfull
         #endregion
 
         private MaterialSkin.Controls.MaterialLabel materialLabel1;
-        private MaterialSkin.Controls.MaterialFlatButton btnCopyCode;
         private MaterialSkin.Controls.MaterialLabel materialLabel2;
         private MaterialSkin.Controls.MaterialLabel materialLabel3;
         private MaterialSkin.Controls.MaterialSingleLineTextField cookieTxt;
@@ -201,6 +233,9 @@ namespace Skillfull
         private System.Windows.Forms.ToolStripStatusLabel statusStripLbl;
         private MaterialSkin.Controls.MaterialLabel materialLabel4;
         private MaterialSkin.Controls.MaterialSingleLineTextField urlTxt;
+        private MaterialSkin.Controls.MaterialLabel materialLabel5;
+        private MaterialSkin.Controls.MaterialRaisedButton btnDownload;
+        private MaterialSkin.Controls.MaterialRaisedButton btnCopyCode;
     }
 }
 
--- Skillfull/Form1.cs
+++ Skillfull/Form1.cs
@@ -17,16 +17,27 @@ namespace Skillfull
             InitializeComponent();
         }
 
-        private void mainForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void btnCopyCode_Click(object sender, EventArgs e)
         {
             Clipboard.SetText("document.cookie");
             statusStripLbl.Text = "Command copied to the clipboard";
-            // document.cookie
+        }
+
+        private void cookieTxt_TextChanged(object sender, EventArgs e)
+        {
+            statusStripLbl.Text = "Cookies entered successfully";
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(cookieTxt.Text) || String.IsNullOrWhiteSpace(cookieTxt.Text))
+            {
+                statusStripLbl.Text = "Cookies must be pressent, please paste them in";
+            }
+            if (String.IsNullOrEmpty(urlTxt.Text) || String.IsNullOrWhiteSpace(urlTxt.Text))
+            {
+                statusStripLbl.Text = "How am I supposed to know which course to download, give me a link you dum dum XD";
+            }
         }
     }
 }
